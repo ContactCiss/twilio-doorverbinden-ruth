a675e3f767a7b67322e4b904ba228b15376220e8
ContactCiss <richard@contactons.nl>
2025-04-25 13:05:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask, request, Response
+from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse, Dial
 
 app = Flask(__name__)
 
-# Zorg ervoor dat de rootroute correct werkt (om 404 te vermijden)
-@app.route("/", methods=["GET"])
-def home():
-    return "Flask app is up and running!"
-
-# Voeg de doorverbinden route toe
-@app.route("/doorverbinden", methods=["POST"])
+@app.route("/doorverbinden", methods=['POST'])
 def doorverbinden():
-    print(f"Ontvangen POST-aanroep: {request.json}")  # Voeg dit toe voor debugging
+    # accepteer form-data van Twilio
     response = VoiceResponse()
-    response.say("Een ogenblikje, ik verbind u nu door naar een medewerker.", language='nl-NL')
-    response.dial("0031884114114", caller_id="+3197010252180")
-    return Response(str(response), mimetype='text/xml')
+    response.say("Een ogenblikje, ik verbind u nu door naar een collega.", language="nl-NL", voice="Polly.Ruben")
+    response.dial("+31630608057")
+    return str(response), 200, {'Content-Type': 'application/xml'}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
